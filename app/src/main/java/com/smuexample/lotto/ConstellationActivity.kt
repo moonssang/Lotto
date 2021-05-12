5f31a7fe2cd0536d83caebc4a831222393ae7747
H-H Moon <moonssang031@naver.com>
2021-05-12 14:28:41 +0900

    Open ConstellationActivity

--- app/src/main/java/com/smuexample/lotto/ConstellationActivity.kt
+++ app/src/main/java/com/smuexample/lotto/ConstellationActivity.kt
@@ -2,10 +2,12 @@ package com.smuexample.lotto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class ConstellationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_constellation)
+
     }
 }
--- app/src/main/java/com/smuexample/lotto/MainActivity.kt
+++ app/src/main/java/com/smuexample/lotto/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.smuexample.lotto
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<View>(R.id.ConStellationCard).setOnClickListener{
+            startActivity(Intent(this,ConstellationActivity::class.java))
+        }
     }
 }
