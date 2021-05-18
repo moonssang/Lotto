00b8b085e1cfa78165c32e353978cac7d998ecc1
H-H Moon <moonssang031@naver.com>
2021-05-18 11:03:59 +0900

    Open NameActivity

--- app/src/main/java/com/smuexample/lotto/MainActivity.kt
+++ app/src/main/java/com/smuexample/lotto/MainActivity.kt
@@ -10,13 +10,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<View>(R.id.ConStellationCard).setOnClickListener{
-            startActivity(Intent(this,ConstellationActivity::class.java))
-
+        findViewById<View>(R.id.ConStellationCard).setOnClickListener {
+            startActivity(Intent(this, ConstellationActivity::class.java))
+        }
             findViewById<View>(R.id.NameCard).setOnClickListener {
                 startActivity(Intent(this,NameActivity::class.java))
             }
-
-        }
     }
 }
