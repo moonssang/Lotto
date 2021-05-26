aede56a0e0205efa09221191ebda89292faf47a3
H-H Moon <moonssang031@naver.com>
2021-05-26 13:25:09 +0900

    Random number generation

--- app/src/main/java/com/smuexample/lotto/MainActivity.kt
+++ app/src/main/java/com/smuexample/lotto/MainActivity.kt
@@ -4,6 +4,36 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import java.util.*
+import kotlin.collections.ArrayList
+
+fun getRandomLottoNumber (): Int{
+    return Random().nextInt(45)+1
+}
+
+fun getRandomLottoNumbers(): MutableList<Int>{
+    val lottoNumbers = mutableListOf<Int>()
+
+    while(true) {
+        var number: Int = getRandomLottoNumber()
+        var flag_existing: Int = 0
+        // for(j in 0..lottoNumbers.size){
+        if (lottoNumbers.contains(number)) {
+            // if(number.equals(lottoNumbers[j])) {
+            flag_existing = 1
+            // continue
+        }
+        //  if(flag_existing.equals(1))
+        //      continue
+        //  else
+        lottoNumbers.add(number)
+        if (lottoNumbers.size >= 6)
+            break;
+        }
+    return lottoNumbers
+    }
+
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,13 +42,14 @@ class MainActivity : AppCompatActivity() {
 
         findViewById<View>(R.id.RandomCard).setOnClickListener {
             startActivity(Intent(this,ResultActivity::class.java))
+            intent.putIntegerArrayListExtra("result", ArrayList(getRandomLottoNumbers()))
         }
 
-        findViewById<View>(R.id.ConStellationCard).setOnClickListener {
+         findViewById<View>(R.id.ConStellationCard).setOnClickListener {
             startActivity(Intent(this, ConstellationActivity::class.java))
         }
 
-        findViewById<View>(R.id.NameCard).setOnClickListener {
+         findViewById<View>(R.id.NameCard).setOnClickListener {
                 startActivity(Intent(this,NameActivity::class.java))
         }
 
